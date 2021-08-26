1e35abf28d9d440951ac63fa1e5fb2751fb14ed7
Vitor Dutra <kaosnoff@gmail.com>
2021-08-26 17:26:25 -0300

    Classe para extração genérica de dados

---
+++ src/ExtraiDadosPdf.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const fs = require('fs');
+const pdfreader = require("pdfreader");
+
+module.exports = class ExtraiDadosPdf
+{
+	raiz = path.join(__dirname,'..');
+	
+	constructor()
+	{
+		console.log('Classe instanciada', this);
+	}
+	
+	extraiArquivo(pathArquivo)
+	{
+		return new Promise((resolve, reject) =>
+		{
+			let filePath = path.join(this.raiz,...pathArquivo.split('/'));
+			
+			let fileSaida = [];
+			let page = 0;
+			
+			try
+			{
+				new pdfreader.PdfReader().parseFileItems(
+					filePath,
+					(err,item) =>
+					{
+						if (err)
+						{
+							console.log(err);
+							reject(err);
+						}
+						else if (!item)
+						{
+							console.log('fim - '+filePath);
+							let newSaida = [];
+							
+							fileSaida.forEach((dados,page) =>
+							{
+								let indices = Object.keys(dados);
+								indices = indices.sort((a,b) => Number(a) > Number(b) ? 1 : -1);
+								//console.log(page,indices)
+								newSaida[page] = {};
+								for (let i of indices)
+								{
+									newSaida[page][Number(i).toFixed(3).toString()] = dados[i];
+								}
+								//console.log(page, indices);
+							})
+							
+							resolve(newSaida);
+						}
+						else if (item.page)
+						{
+							page = item.page - 1;
+							fileSaida[page] = {};
+						}
+						else if (item.text)
+						{
+							//console.log(item.y,item.text);
+							let y = item.y;
+							if (fileSaida[page][y] === undefined) fileSaida[page][y] = [];
+							fileSaida[page][y].push(item.text);
+						}
+					}
+				);
+			}
+			catch(e)
+			{
+				reject({msg: "Arquivo não encontrado",e});
+			}
+		});
+		
+	}
+	
+	processaDiretorio(pathDir, funcao)
+	{
+		return new Promise((resolve, reject) =>
+		{
+			let dir;
+			let fullPath = path.join(this.raiz,...pathDir.split('/'));
+			let promises = [];
+			try
+			{
+				dir = fs.readdirSync(fullPath);
+			}
+			catch(e)
+			{
+				reject({msg: "Diretório não encontrado",e});
+			}
+			dir.forEach((fileName) =>
+			{
+				let ext = fileName.split('.').length > 1 ? fileName.split('.').pop() : undefined;
+				if (ext != 'pdf') return;
+				
+				let retorno = new Promise((resolve1,reject1) =>
+				{
+					this.extraiArquivo(path.join(...pathDir.split('/'),fileName))
+					.then(paginas =>
+					{
+						resolve1(funcao(paginas));
+					})
+					.catch(e => reject1(e));
+				})
+				promises.push(retorno);
+				//console.log(path.join(...pathDir.split('/'),fileName))
+				//console.log(fileName, ext);
+			});
+			Promise.all(promises)
+			.then(multidados =>
+			{
+				console.log(multidados);
+			})
+			.catch(e => reject(e));
+		});
+	}
+}
